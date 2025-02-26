a0ccc06a6f3bb2972c5997310650b9d5b143254a
bynd1u <semenbindu@gmail.com>
2025-02-26 18:13:22 +0100

    Add Todo model with schema definition for text, completed status, and user reference

---
+++ server/models/todo.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+export default Todo;
