27b07672de07eeb65e9554e77a72e1799a194dcd
bynd1u <semenbindu@gmail.com>
2025-02-27 13:40:42 +0100

    mongodb connected

---
+++ server/config/db.js
@@ -0,0 +1,20 @@
+    import mongoose from 'mongoose';
+    import dotenv from 'dotenv';
+    import path , { dirname } from 'path';
+    import { fileURLToPath } from 'url';
+
+    // construct path 
+    const __filename = fileURLToPath(import.meta.url);
+    const PATH = dirname(__filename);
+    // Load env variables
+    let result = dotenv.config({ path: path.join(PATH, '..', '.env') });
+    const connectToDB = async () => {
+        try {
+            await mongoose.connect(process.env.CONNECTION_URI);
+            console.log('Connected to the database successfully');
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    export default connectToDB;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import connectToDB from './config/db.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ const app = express();
 
 //app.use(express.static(path.join(PATH, '..', 'client')));
 
+connectToDB();
 
 
 app.listen(PORT, () => {
