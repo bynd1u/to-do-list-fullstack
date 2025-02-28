a5787dc546f1826f9e294d9444a804ba3934d137
bynd1u <semenbindu@gmail.com>
2025-02-28 18:35:42 +0100

    user registration form

--- server/index.js
+++ server/index.js
@@ -3,11 +3,9 @@ import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
 import connectToDB from './config/db.js';
 import Todo from './models/todo.js'
-import bodyParser from 'body-parser';
-import { create } from 'domain';
+import User from './models/user.js';
 
 
 dotenv.config();
@@ -17,7 +15,10 @@ const __filename = fileURLToPath(import.meta.url);
 const PATH = dirname(__filename);
 
 const app = express();
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
 //app.use(express.static(path.join(PATH, '..', 'client')));
@@ -56,6 +57,15 @@ app.delete('/all', async (req, res) => {
 app.get('/', (req, res) => {
   res.send("Hi weirdo");
 })
+
+app.post('/register', async (req, res) => {
+  //responde with body
+  const newUser = new User(req.body);
+
+  await newUser.save();
+
+  res.send(newUser);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
